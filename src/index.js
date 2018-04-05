24a24ebf15f2ae50693967bad20b3d5161ec2bda
LuisR <luisrojasch.29@gmail.com>
2018-04-05 16:07:00 -0500

    Se termina github list ejemplo...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import moment from 'moment';
 import './index.css';
+import PropTypes from 'prop-types';
+import Time from './time';
 
-ReactDOM.render(</>, document.querySelector('#root'));
+const FileList = ( {files} ) => {
+
+    return(
+        <table className="file-list">
+            <tbody>
+            {files.map(file=>(
+                <FileListItem key={file.id} file={file}/>
+            ))}
+            </tbody>
+        </table>
+    )
+
+}
+
+FileList.propTypes = {
+    files: PropTypes.array
+};
+
+const FileListItem = ( {file} ) => (
+    <tr className="file-list-item">
+        {getFileName(file)}
+        <CommitMessage commit={file.latestCommit} />
+        <td className="age">
+            <Time time={file.updated_at}/>
+        </td>
+    </tr>
+)
+
+FileListItem.propTypes = {
+    file: PropTypes.object.isRequired
+}
+
+function FileIcon( {file} ){
+    let icon = 'fa-file-text-o';
+    if( file.type === 'folder' ){
+        icon = 'fa-folder'
+    }
+
+    return(
+        <td className="file-icon">
+            <i className={`fa ${icon}`}></i>
+        </td>
+    )
+}
+
+FileIcon.propTypes = {
+    file:PropTypes.object.isRequired
+}
+
+const CommitMessage = ({commit}) =>(
+    <td className="commit-message">
+        {commit.message}
+    </td>
+)
+
+CommitMessage.propTypes = {
+    commit: PropTypes.object.isRequired
+}
+
+function getFileName(file){
+    return[
+        <FileIcon file={file} key={0}/>,
+        <td className="file-name" key={1}>{file.name}</td>
+    ];
+}
+
+const testFiles = [
+    {
+        id: 1,
+        name: 'src',
+        type: 'folder',
+        updated_at: "2016-07-11 21:24:00",
+        latestCommit: {
+            message: 'Initial commit'
+        }
+    },
+    {
+        id: 2,
+        name: 'tests',
+        type: 'folder',
+        updated_at: "2016-07-11 21:24:00",
+        latestCommit: {
+            message: 'Initial commit'
+        }
+    },
+    {
+        id: 3,
+        name: 'README',
+        type: 'file',
+        updated_at: "2016-07-18 21:24:00",
+        latestCommit: {
+            message: 'Added a readme'
+        }
+    },
+];
+
+ReactDOM.render(<FileList files={testFiles}/>, document.querySelector('#root'));
