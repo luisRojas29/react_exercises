d65137d75f1f68c7b191b454d0f24252837ca1cd
Luis Rojas <luisrojasch.29@gmail.com>
2018-04-16 11:44:35 -0500

    Modificaciones del ejercicio...

--- src/index.js
+++ src/index.js
@@ -2,22 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Pin = ({pinte}) =>{
-    return(
-        <div className="pin">
-            <h2 className="title">{pinte.title}</h2>
-            <p className="author">{pinte.author}</p>
-            <Mosaic imgs={pinte.imgs} />
-            <FollowButton />
+const PinteresCopy = () => {
 
+    return(
+        <div className="pinterest">
+            <PinHeader />
+            <PinInfo />
+            <div> 
+                
+            </div>
+        </div>
+    )
+
+}
+
+const PinHeader = () => {
+    return(
+        <div className="header">
+            <img src="http://dummyimage.com/100x100/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" className="imageHeader" />
+            <h1>Author</h1>
         </div>
     )
 }
 
-const Mosaic = ({imgs}) => {
+const PinInfo = () => {
+    return(
+        <div className="infoHeader">
+            <ul>
+                <li>
+                    <div className="info-item">
+                        <p>
+                           37
+                           <span>Boards</span> 
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div className="info-item">
+                    </div>
+                </li>
+                <li>
+                    <div className="info-item">
+                    </div>
+                </li>
+                <li>
+                    <div className="info-item">
+                    </div>
+                </li>
+                <li>
+                    <div className="info-item">
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+const Pin = ({pinte}) => {
+    return(
+        <div className="pin">
+            <p className="title">{pinte.title}</p>
+            <p className="author">{pinte.author}</p>
+            <Mosaic imgs={pinte.imgs} num={pinte.num_pins} />
+            <FollowButton />
+        </div>
+    )
+}
+
+const Mosaic = ({imgs, num}) => {
     return(
         <div className="mosaic">
             <div className="main-img">
+                <div className="num-pins">
+                    <i className="fa fa-thumb-tack" />
+                    <span>{num}</span>
+                </div>
                 <img src={imgs[0]} alt=""/>
             </div>
             <div className="sub-img">
@@ -39,7 +99,8 @@ const pinte = {
         "http://via.placeholder.com/350x150?text=1",
         "http://via.placeholder.com/350x150?text=2",
         "http://via.placeholder.com/350x150?text=3",
-    ]
+    ],
+    num_pins:4850
 
 }
 
