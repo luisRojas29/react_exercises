5868a6962f2a24fb6122cf3484b61252124d1456
Luis Rojas <luisrojasch.29@gmail.com>
2018-04-23 23:57:11 -0500

    Ejemplo de state...

--- src/index.js
+++ src/index.js
@@ -3,4 +3,45 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 import './index.css';
 
-ReactDOM.render(</>, document.querySelector('#root'));
+class CountingParent extends React.Component{
+
+	state = {
+		actionCount: 0
+	}
+
+	handleAction = (action) => {
+		console.log("child did: " + action);
+
+		this.setState({
+			actionCount : this.state.actionCount + 1
+		});
+	}
+
+	reset = () =>{
+		this.setState({
+			actionCount : 0
+		});
+	}
+
+	render(){
+		return(
+			<div>
+				<Child onAction={this.handleAction} onReset={this.reset} />
+				
+				<p>Clicked {this.state.actionCount} times </p>
+			</div>
+		);
+	}
+
+}
+
+function Child({onAction, onReset}){
+	return(
+		<div>
+			<button onClick={onAction}>Click Me!</button>
+			<button onClick={onReset}>Reset </button>
+		</div>
+	)
+}
+
+ReactDOM.render(<CountingParent/>, document.querySelector('#root'));
